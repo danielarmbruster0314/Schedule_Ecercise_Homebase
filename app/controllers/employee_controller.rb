54e8ce1f8a85c704837cac3a78c749af85a57ad2
Daniel Armbruster <danielgundam00@gmail.com>
2022-06-09 16:54:39 -0600

    saving new changes to the models

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,6 +1,10 @@
 class EmployeeController < ApplicationController
   def index
-    employees = Employee.all.sort_by
+    if params[:sort_by][:first_name]
+    employees = Employee.all.order_by(:first_name)
     render json: employees
-  end
+    elsif params[:sort_by][:last_name]
+      employees = Employee.all.order_by(:last_name)
+      render json: employees
+    end    
 end
