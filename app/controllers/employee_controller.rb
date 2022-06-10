75bc14a02788ea49cf12ac1cc51b1d0e4d2b6eef
Daniel Armbruster <danielgundam00@gmail.com>
2022-06-09 21:08:30 -0600

    seeded data and got it to send data in the pattern specified

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,10 +1,12 @@
 class EmployeeController < ApplicationController
   def index
-    if params[:sort_by][:first_name]
-    employees = Employee.all.order_by(:first_name)
+   
+    if (params[:sort_by] === "first_name")
+    employees = Employee.all.order(:first_name)
     render json: employees
-    elsif params[:sort_by][:last_name]
-      employees = Employee.all.order_by(:last_name)
+    elsif (params[:sort_by] === "last_name")
+      employees = Employee.all.order(:last_name)
       render json: employees
-    end    
+    end   
+  end  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,117 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+person_1 = Employee.create(first_name: "Alfred", last_name: "Brown")
+person_2 = Employee.create(first_name: "Tim", last_name: "Cannady")
+person_3 = Employee.create(first_name: "Jeff", last_name: "Auston")
+
+
+
+shift_1 = Shift.create(
+        day: 0,
+        start_at: "12pm",
+        end_at: "5pm",
+        duration: 5,
+        role: "Server",
+        color: "red",
+        employee_id: person_1.id
+)
+shift_2 = Shift.create(
+        day: 1,
+        start_at: "9am",
+        end_at: "12pm",
+        duration: 3,
+        role: "Host",
+        color: "green",
+        employee_id: person_1.id
+)
+shift_2 = Shift.create(
+        day: 3,
+        start_at: "9am",
+        end_at: "4pm",
+        duration: 7,
+        role: "Server",
+        color: "red",
+        employee_id: person_1.id
+)
+shift_3 = Shift.create(
+        day: 5,
+        start_at: "9am",
+        end_at: "2pm",
+        duration: 5,
+        role: "Host",
+        color: "green",
+        employee_id: person_1.id
+)
+shift_4 = Shift.create(
+        day: 0,
+        start_at: "11am",
+        end_at: "6pm",
+        duration: 7,
+        role: "Chef",
+        color: "orange",
+        employee_id: person_2.id
+)
+shift_5 = Shift.create(
+        day: 1,
+        start_at: "9am",
+        end_at: "3pm",
+        duration: 6,
+        role: "Dishwasher",
+        color: "purple",
+        employee_id: person_2.id
+)
+shift_6 = Shift.create(
+        day: 2,
+        start_at: "9am",
+        end_at: "1pm",
+        duration: 4,
+        role: "Chef",
+        color: "orange",
+        employee_id: person_2.id
+)
+shift_6 = Shift.create(
+        day: 5,
+        start_at: "9pm",
+        end_at: "4am",
+        duration: 7,
+        role: "Dishwasher",
+        color: "purple",
+        employee_id: person_2.id
+)
+shift_7 = Shift.create(
+        day: 1,
+        start_at: "11am",
+        end_at: "6pm",
+        duration: 7,
+        role: "Chef",
+        color: "orange",
+        employee_id: person_3.id
+)
+shift_8 = Shift.create(
+        day: 2,
+        start_at: "9am",
+        end_at: "3pm",
+        duration: 6,
+        role: "Dishwasher",
+        color: "purple",
+        employee_id: person_3.id
+)
+shift_9 = Shift.create(
+        day: 4,
+        start_at: "9am",
+        end_at: "1pm",
+        duration: 5,
+        role: "Chef",
+        color: "orange",
+        employee_id: person_3.id
+)
+shift_10 = Shift.create(
+        day: 6,
+        start_at: "9am",
+        end_at: "4pm",
+        duration: 7,
+        role: "Dishwasher",
+        color: "purple",
+        employee_id: person_3.id
+)
